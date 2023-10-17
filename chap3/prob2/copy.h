76d9b6f877bcf137de6b1a7121dab0964ea8011a
hhh1883 <hhh1883@naver.com>
2023-10-17 07:07:14 +0000

    test4

--- chap3/prob2/copy.h
+++ chap3/prob2/copy.h
@@ -1,3 +1,3 @@
-   #define MAXLINE 100
-   void copy(char from[], char to[]);
+#define MAXLINE 100
+void copy(char from[], char to[]);
 
--- chap3/prob2/main.c
+++ chap3/prob2/main.c
@@ -1,27 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include "copy.h"
+char line[MAXLINE];
+char longest[MAXLINE];
+void copy(char from[], char to[]);
 
-   #include <stdio.h>
-   #include <string.h>
-   #include "copy.h"
-   char line[MAXLINE];
-   char longest[MAXLINE];
-   void copy(char from[], char to[]);
-  
-  
-   int main()
-  {
-    int len, max = 0;
- 
-      while (fgets(line,MAXLINE,stdin) != NULL) {
-          len = strlen(line);
-          if(len > max) {
-              max = len;
-              copy(line, longest);
-          }
-      }
-      if (max > 0)
-      printf("%s", longest);
- 
-      return 0;
- 
-  }
+
+int main() 
+{
+	int len, max = 0;
+	int n = 0;
+	while (n < 5 && fgets(line,MAXLINE,stdin) != NULL) {
+		len = strlen(line);
+		if(len > max) {
+			max = len;
+			copy(line, longest);
+		}
+		n++;
+	}
+	if (max > 0)
+	printf("%s", longest);
+
+	return 0;
+
+}
 
