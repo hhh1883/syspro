916a0c890989fe5c29bfb25c707cd532dde9fad3
hhh1883 <hhh1883@naver.com>
2023-10-19 04:47:09 +0000

    test5

--- chap4/prob4/main.c
+++ chap4/prob4/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 	fp = fopen(argv[1], "w");
 	printf("%-9s %-7s %-4s\n", "StudentID", "Name", "Score");
 	while (scanf("%d %s %d", &rec.id, rec.name, &rec.score)==3)
-		fprintf(fp, "%d %s %d", rec.id, rec.name,rec.score);
+		fprintf(fp, "%d %s %d\n", rec.id, rec.name,rec.score);
 	fclose(fp);
 	return 0;
 }
